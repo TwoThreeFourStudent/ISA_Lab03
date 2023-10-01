2f8f5f3fa9b6e5b1b242bc275b3e12adf91486fa
Miguel Magpoc <mmagpoc@my.bcit.ca>
2023-09-30 21:17:09 -0700

    name and server time is displayed but not coloured yet

---
+++ index.js
@@ -0,0 +1,15 @@
+const dt = require('./modules/date')
+const http = require('http');
+const fs = require('fs');
+let url = require('url')
+
+
+http.createServer(function (request, response) {
+    let serverTime = dt.getDate()
+    let q = url.parse(request.url,true)
+    let name = q.query["name"]
+    response.writeHead(200,{"Content-Type":"text/html"})
+    response.end(`Hello ${name}, What a beautiful day. Server current date and time is ${serverTime}`)
+}
+).listen(8080);
+console.log('listening ...');
---
+++ modules/date.js
@@ -0,0 +1,1 @@
+exports.getDate = () => Date()
