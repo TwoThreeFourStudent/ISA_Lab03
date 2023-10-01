0e145df613bb2486e07c176f30eb35cad79513e3
Miguel Magpoc <mmagpoc@my.bcit.ca>
2023-09-30 19:02:47 -0700

    first part

---
+++ testWithModuel.js
@@ -0,0 +1,3 @@
+const mathMod = require('/Users/miguelmagpoc/COMP4537/labs/3/ISA_Lab03/modules/math.js')
+
+console.log(`This is Miguel Magpoc and Kenar Crasto. Addition: ${mathMod.add(1, 2)}, Subtraction is ${mathMod.sub(2, 1)}`)
